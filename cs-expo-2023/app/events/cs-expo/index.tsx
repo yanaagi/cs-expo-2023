b7b68acb346ceec86da425ae3f1e0853aff5c45f
jrchjhn <almorojericho14@gmail.com>
2023-10-20 19:44:29 +0800

    Commit for CS-EXPO and Dev-Day Back-end

--- cs-expo-2023/app/events/cs-expo/index.tsx
+++ cs-expo-2023/app/events/cs-expo/index.tsx
@@ -1,8 +1,64 @@
 const EventsCsExpo: React.FC = () => {
     return (
-        <div>
-            <p>cs expo</p>
-        </div>
+        <main>
+            <div className = "NavBar">
+                {/*INSERT NAV BAR HERE*/}
+            </div>
+
+            <div className = "CS-Expo">
+                {/*INSERT PHOTO HERE*/}
+                {/*INSERT LOREM IPSUM FLOATING TEXT*/}
+                <p>CS Expo</p>
+            </div>
+
+            <div className = "About">
+                <p>About</p>
+                {/*INSERT LOREM IPSUM - DESC*/}
+                {/*INSERT IMAGE TO THE RIGHT*/}
+            </div>
+
+            <div className = "Panelists">
+                <p>Panelists</p>
+                {/*INSERT NAME - PANNELIST*/}
+                {/*INSERT SHORT DESC - PANNELIST*/}
+
+                {/*INSERT IMAGE PANNELIST*/}
+                {/*INSERT PANNELIST VERTICAL SCROLL*/}
+            </div>
+
+            <div className = "Previous">
+                <p>Previous</p>
+                {/*INSERT YEAR 2022*/}
+                {/*DESC*/}
+                {/*INSERT IMAGE EVENTS CAROUSEL*/}
+
+                {/*INSERT YEAR 2021*/}
+                {/*DESC*/}
+                {/*INSERT IMAGE EVENTS CAROUSEL*/}
+
+                {/*INSERT YEAR 2020*/}
+                {/*DESC*/}
+                {/*INSERT IMAGE EVENTS CAROUSEL*/}
+
+                {/*INSERT YEAR 2019*/}
+                {/*DESC*/}
+                {/*INSERT IMAGE EVENTS CAROUSEL*/}
+
+                {/*INSERT YEAR 2018*/}
+                {/*DESC*/}
+                {/*INSERT IMAGE EVENTS CAROUSEL*/}
+
+                {/*INSERT YEAR 2017*/}
+                {/*DESC*/}
+                {/*INSERT IMAGE EVENTS CAROUSEL*/}  
+            </div>
+
+
+            <div className = "Footer">
+                {/*INSERT FOOTER*/}
+            </div>
+
+        </main>
     );
 };
 
--- cs-expo-2023/app/events/dev-day/index.tsx
+++ cs-expo-2023/app/events/dev-day/index.tsx
@@ -1,8 +1,64 @@
 const EventsDevDay: React.FC = () => {
     return (
-        <div>
-            <p>dev day</p>
-        </div>
+        <main>
+            <div className = "NavBar">
+                {/*INSERT NAV BAR HERE*/}
+            </div>
+
+            <div className = "Dev-Day">
+                {/*INSERT PHOTO HERE*/}
+                {/*INSERT LOREM IPSUM FLOATING TEXT*/}
+                <p>Dev Day</p>
+            </div>
+
+            <div className = "About">
+                <p>About</p>
+                {/*INSERT LOREM IPSUM - DESC*/}
+                {/*INSERT IMAGE TO THE RIGHT*/}
+            </div>
+
+            <div className = "Speakers">
+                <p>Speakers</p>
+                {/*INSERT NAME - PANNELIST*/}
+                {/*INSERT SHORT DESC - PANNELIST*/}
+
+                {/*INSERT IMAGE PANNELIST*/}
+                {/*INSERT PANNELIST VERTICAL SCROLL*/}
+            </div>
+
+            <div className = "Previous">
+                <p>Previous</p>
+                {/*INSERT YEAR 2022*/}
+                {/*DESC*/}
+                {/*INSERT IMAGE EVENTS CAROUSEL*/}
+
+                {/*INSERT YEAR 2021*/}
+                {/*DESC*/}
+                {/*INSERT IMAGE EVENTS CAROUSEL*/}
+
+                {/*INSERT YEAR 2020*/}
+                {/*DESC*/}
+                {/*INSERT IMAGE EVENTS CAROUSEL*/}
+
+                {/*INSERT YEAR 2019*/}
+                {/*DESC*/}
+                {/*INSERT IMAGE EVENTS CAROUSEL*/}
+
+                {/*INSERT YEAR 2018*/}
+                {/*DESC*/}
+                {/*INSERT IMAGE EVENTS CAROUSEL*/}
+
+                {/*INSERT YEAR 2017*/}
+                {/*DESC*/}
+                {/*INSERT IMAGE EVENTS CAROUSEL*/}  
+            </div>
+
+
+            <div className = "Footer">
+                {/*INSERT FOOTER*/}
+            </div>
+
+        </main>
     );
 };
 
