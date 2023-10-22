583cf5f81c11d86c3bdb23520f72a514efedfdb6
Reggie Guday <coderej16@gmail.com>
2023-10-22 19:14:20 +0800

    Add colections of projects

---
+++ cs-expo-2023/app/projects/collections-of-projects/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CallectionOfProjects = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>Collection of projects</div>
+    </main>
+  )
+}
+
+export default CallectionOfProjects
