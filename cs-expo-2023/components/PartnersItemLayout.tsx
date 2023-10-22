93ff7a59f02adcd220b0ea05ca5e18648c537a3d
Reggie Guday <coderej16@gmail.com>
2023-10-22 11:43:39 +0800

    Add Partners Icon

---
+++ cs-expo-2023/components/PartnersItemLayout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from 'next/image'
+
+export interface PartnersInterface {
+  src: string;
+}
+
+const PartnerItemLayout = (partner:PartnersInterface) => {
+  return (
+    <div class="inline-block ml-1">
+      <Image class="h-auto max-w-full rounded-full" src={partner.src} width={200} height={200}/>
+    </div>
+  );
+} 
+
+export default PartnerItemLayout;
