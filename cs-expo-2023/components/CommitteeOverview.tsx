dbe1229a023cd18ab525924dfc1492e4a88c0fab
Dale0122 <asdale0122@gmail.com>
2023-10-23 02:24:22 +0800

    feat: Created Committees Overview component

---
+++ cs-expo-2023/components/CommitteeOverview.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const CommitteeOverview = () => {
+  return (
+    <main className=''>
+      <div className='text-center'>
+        <h1 className="text-8xl text-eerie-black mb-4 font-bold">COMMITTEES</h1>
+        <p className='text-xl text-eerie-black font-helvetica max-w-xl mx-auto mb-24'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mattis convallis ex, eget malesuada ligula fermentum 
+          cursus. Suspendisse a aliquam sem, at condimentum dolor. Aliquam sed pulvinar lectus. Nam ullamcorper est metus, quis faucibus 
+          neque ornare a. Phasellus id faucibus lorem. Donec eu luctus leo. Aenean pulvinar ligula eu elit rhoncus, ut ullamcorper odio 
+          vehicula.
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default CommitteeOverview;
