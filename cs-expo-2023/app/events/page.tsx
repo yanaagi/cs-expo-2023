a14afc7f5b0ac072fb7c77b68458578dea8f5fba
Dale0122 <asdale0122@gmail.com>
2023-10-23 01:57:16 +0800

    feat+refactor: Edited page for events overview and displayed other components while buttons are not yet done

--- cs-expo-2023/app/events/page.tsx
+++ cs-expo-2023/app/events/page.tsx
@@ -1,17 +1,31 @@
 import React from 'react'
+import CSExpoOverview from '@/components/CSExpoOverview';
+import DevDayOverview from '@/components/DevDayOverview';
+import CommitteeOverview from '@/components/CommitteeOverview';
 
 export const EventsOverview = () => {
   return (
-    <main className='flex min-h-screen items-center justify-center mb-lg'>
+    <main className='flex flex-col min-h-screen items-center justify-center p-24 mt-24'>
         <div className='text-center'>
           <h1 className="text-8xl text-eerie-black mb-4 font-bold">EVENTS</h1>
-          <p className='text-xl text-eerie-black font-helvetica max-w-xl mx-auto'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mattis convallis ex, eget malesuada ligula fermentum 
+          <p className='text-xl text-eerie-black font-helvetica max-w-xl mx-auto mb-24'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mattis convallis ex, eget malesuada ligula fermentum 
              cursus. Suspendisse a aliquam sem, at condimentum dolor. Aliquam sed pulvinar lectus. Nam ullamcorper est metus, quis faucibus 
              neque ornare a. Phasellus id faucibus lorem. Donec eu luctus leo. Aenean pulvinar ligula eu elit rhoncus, ut ullamcorper odio 
              vehicula.</p>
         </div>
+        <div className=''>
+          <CSExpoOverview />
+        </div>
+        <div className=''>
+          <DevDayOverview />
+        </div>
+        <div className=''>
+          <CommitteeOverview />
+        </div>
     </main>
   )
 }
+
+
 
 export default EventsOverview
