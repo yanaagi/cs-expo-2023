24cba6d3a8aec3fb9d51329f34f43912d2abb488
rlme17 <richespino17@gmail.com>
2023-10-23 22:30:59 +0800

    fix: UPDATE ProjectsHeader.tsx and page.tsx

---
+++ cs-expo-2023/components/ProjectsHeader.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const ProjectsHeader = () => {
+  return (
+    <main>
+      
+        <div className="w-full self-center relative">
+            <div className="w-full top-0 text-center text-zinc-900 text-5xl font-bold font-['Helvetica Now Text ']">PROJECTS</div>
+            <div className="flex w-4/12 mx-auto text-center text-zinc-900 text-xl font-normal font-['Helvetica Now Text ']">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl purus in mollis nunc sed id semper risus. Volutpat consequat mauris nunc congue nisi vitae suscipit tellus.</div>
+        </div>
+    </main>
+  );
+};
+export default ProjectsHeader;
