2eb7661ebe9c90c4a3f5960b26cf08d3cc32d0ca
rlme17 <richespino17@gmail.com>
2023-10-23 22:55:27 +0800

    fix: UPDATE the ProjectsHeader.tsx and projects/page.tsx

--- cs-expo-2023/app/projects/page.tsx
+++ cs-expo-2023/app/projects/page.tsx
@@ -7,7 +7,7 @@ import ThesisPosterCard from '@/components/ThesisPosterCard';
 const Projects = () => {
   return (
   <main className='flex flex-col items-center justify-between p-24'>
-    <ProjectsHeader/> 
+    <ProjectsHeader headerText ="PROJECTS" subheaderText="LOREM"/> 
     <div className="flex justify-center p-4 w-full">
       <Link href="/projects/hall-of-fame" className="flex items-center cursor-pointer ml-4 font-medium text-coral-pink">Hall of Fame <span className="flex-auto"><AiOutlineRight /></span></Link>
       <Link href="/projects/collections-of-projects" className="flex items-center cursor-pointer ml-4 font-medium text-coral-pink">Collections of Projects <span><AiOutlineRight /></span></Link>
--- cs-expo-2023/components/ProjectsHeader.tsx
+++ cs-expo-2023/components/ProjectsHeader.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
+interface ProjectsHeaderProps {
+    headerText: string;
+    subheaderText: string;
+}
 
-const ProjectsHeader = () => {
+const ProjectsHeader: React.FC<ProjectsHeaderProps> = ({ headerText, subheaderText}) => {
   return (
     <main>
-      
+
         <div className="w-full self-center relative">
-            <div className="w-full top-0 text-center text-zinc-900 text-5xl font-bold font-['Helvetica Now Text ']">PROJECTS</div>
-            <div className="flex w-4/12 mx-auto text-center text-zinc-900 text-xl font-normal font-['Helvetica Now Text ']">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl purus in mollis nunc sed id semper risus. Volutpat consequat mauris nunc congue nisi vitae suscipit tellus.</div>
+            <div className="w-full top-0 text-center text-zinc-900 text-5xl font-bold font-['Helvetica Now Text ']">
+              {headerText}
+            </div>
+            <div className="flex w-4/12 mx-auto text-center text-zinc-900 text-xl font-normal font-['Helvetica Now Text ']">
+              {subheaderText}
+            </div>
         </div>
     </main>
   );
