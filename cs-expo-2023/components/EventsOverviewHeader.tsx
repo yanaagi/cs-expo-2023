ee1d6efde1949f935173d40b170fa830d4ff41fc
Dale0122 <asdale0122@gmail.com>
2023-10-23 23:30:05 +0800

    feat: Edited Events Overview page and added componentt

---
+++ cs-expo-2023/components/EventsOverviewHeader.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface EventsOverviewHeaderProps {
+    headerText: string;
+    subheaderText: string;
+}
+
+const EventsHeader: React.FC<EventsOverviewHeaderProps> = ({ headerText, subheaderText }) => {
+  return (
+    <main className='flex min-h-screen items-center justify-center mb-lg'>
+        <div className='text-center'>
+          <h1 className="text-8xl text-eerie-black mb-4 font-bold">{headerText}</h1>
+          <p className='text-xl text-eerie-black font-helvetica max-w-xl mx-auto'>{subheaderText}</p>
+        </div>
+    </main>
+  )
+}
+
+export default EventsHeader;git fetch origin
