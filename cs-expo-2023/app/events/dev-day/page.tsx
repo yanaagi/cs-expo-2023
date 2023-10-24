1f8d22ed687ca5290013ab47f8251b4f14320e97
Zeno Donato <zeno16.donato@gmail.com>
2023-10-24 18:45:02 +0800

    feat: CHANGE dev day page.tsx

--- cs-expo-2023/app/events/dev-day/page.tsx
+++ cs-expo-2023/app/events/dev-day/page.tsx
@@ -1,8 +1,18 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import PhotoComponent from "@/components/PhotoComponent";
+import AboutComponent from "@/components/AboutComponent";
+import ButtonComponent from "@/components/ButtonComponent";
+
 
 export const EventsDevDay = () => {
+    const [currentButton, setCurrentButton] = useState(1);
+
+    const changeButton = (buttonNumber) => {
+        setCurrentButton(buttonNumber);
+    };
+
     useEffect(() => {
         const config = {
             type: "carousel",
@@ -11,12 +21,29 @@ export const EventsDevDay = () => {
         };
         new Glide(".carousel-2022", config).mount();
         new Glide(".carousel-2023", config).mount();
-    });
+    }, []); 
 
     return (
         <main className="flex min-h-screen flex-col p-24">
-            <div className="container"></div>
-            <div className="ms-28">
+        <div className="container">
+            <PhotoComponent 
+                    currentButton={currentButton}
+                    customText="DEV DAY Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    sunt in culpa qui officia deserunt mollit anim id est laborum."
+                />
+            <ButtonComponent 
+                    currentButton={currentButton} 
+                    changeButton={changeButton} 
+                />
+                <h1 className="font-black text-8xl text-center mb-20">DEV DAY</h1>
+                <hr className="border-t-1 border-black mb-4" />
+            <AboutComponent
+                    customText="DEV DAY Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                    sampleText="Sample Photo"
+                />
                 <h1 className="font-black text-8xl">PREVIOUS DEV DAYS</h1>
                 <div className="grid grid-cols-12 mt-8 me-12">
                     <div className="col-span-3">
