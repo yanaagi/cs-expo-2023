aa8c9027e015b7c6089e34f3406bd30ae86c8623
nchlnnmrvnz <avanez.nichole@gmail.com>
2023-10-24 20:52:22 +0800

    fix: UPDATE NavBar.tsx

--- cs-expo-2023/components/NavBar.tsx
+++ cs-expo-2023/components/NavBar.tsx
@@ -1,5 +1,5 @@
 'use client'
-'use client'
+
 import React, { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -14,8 +14,8 @@ const [showMobileEventsDropdown, setShowMobileEventsDropdown] = useState(false);
 const [showMobileProjectsDropdown, setShowMobileProjectsDropdown] = useState(false);
 const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
 const dropdownRef = useRef<HTMLDivElement | null>(null);
-const ref = useRef(null);
 const [isNavbarAtTop, setIsNavbarAtTop] = useState(true);
+const mobileNavbarRef = useRef<HTMLDivElement | null>(null);
 
 const toggleEventsDropdown = () => {
      setShowEventsDropdown(!showEventsDropdown);
@@ -35,7 +35,7 @@ const handleDropdownLinkClick = () => {
 const toggleMobileEventsDropdown = () => {
      setShowMobileEventsDropdown(!showMobileEventsDropdown);
 };
- 
+
 const toggleMobileProjectsDropdown = () => {
      setShowMobileProjectsDropdown(!showMobileProjectsDropdown);
 };
@@ -46,36 +46,43 @@ const handleMobileDropdownLinkClick = () => {
 };
 
 const toggleMobileMenu = () => {
-  if (isNavbarAtTop) {
-    setShowMobileEventsDropdown(false);
-    setShowMobileProjectsDropdown(false);
-    setMobileMenuVisible(!mobileMenuVisible);
-  } else {
-    setMobileMenuVisible(!mobileMenuVisible);
-  }
+     setShowMobileEventsDropdown(false);
+     setShowMobileProjectsDropdown(false);
+     setMobileMenuVisible(!mobileMenuVisible);
 };
 
-   
-   
-
 useEffect(() => {
-     const handleOutsideClick = (event: MouseEvent) => {
-          if (dropdownRef.current && !dropdownRef.current.contains(event.target as HTMLElement)) {
-               handleDropdownLinkClick();
-               handleMobileDropdownLinkClick();
-          }
-     };
+	const handleOutsideClick = (event: MouseEvent) => { 
+		if (mobileNavbarRef.current && !mobileNavbarRef.current.contains(event.target as HTMLElement)) {
+			handleMobileDropdownLinkClick();
+			toggleMobileMenu();
+			setMobileMenuVisible(false);
+		}
+          
+		if (dropdownRef.current && !dropdownRef.current.contains(event.target as HTMLElement)) {
+			handleDropdownLinkClick();
+		}
+	};
 
-     document.addEventListener('mousedown', handleOutsideClick);
+	document.addEventListener('mousedown', handleOutsideClick);
 
-     return () => {
-          document.removeEventListener('mousedown', handleOutsideClick);
-          };
-}, [ref]);
+	return () => {
+		 document.removeEventListener('mousedown', handleOutsideClick);
+	};
+}, [dropdownRef, mobileNavbarRef]);
 
 useEffect(() => {
      const handleScroll = () => {
-          setIsNavbarAtTop(window.scrollY === 0);
+          const isAtTop = window.scrollY === 0;
+          setIsNavbarAtTop(isAtTop);
+
+          if (isAtTop && mobileMenuVisible) {
+               setShowEventsDropdown(false);
+               setShowProjectsDropdown(false);
+               setShowMobileEventsDropdown(false);
+               setShowMobileProjectsDropdown(false);
+               setMobileMenuVisible(false);
+          }
      };
 
      window.addEventListener('scroll', handleScroll);
@@ -83,7 +90,7 @@ useEffect(() => {
      return () => {
           window.removeEventListener('scroll', handleScroll);
      };
-}, []);
+}, [mobileMenuVisible]);
 
 const defaultMenu = () => {
      return (
@@ -98,7 +105,7 @@ const defaultMenu = () => {
                     </button>
                     
                     {showEventsDropdown && (
-                         <div ref={dropdownRef} className="absolute mt-2 border rounded-md text-left font-normal w-max">
+                         <div ref={dropdownRef} className="absolute mt-2 bg-white border rounded-md text-left font-normal w-max">
                               <Link href="/events" className="block px-4 py-2 hover:text-coral-pink" onClick={handleDropdownLinkClick}>
                                    Overview
                               </Link>
@@ -153,91 +160,92 @@ const defaultMenu = () => {
 const mobileMenu = () => {
      return (
           <div className="mobile-menu flex flex-col px-6 py-3 items-start font-semibold text-timberwolf">
-          <div className="relative group">
-               <button onClick={toggleMobileEventsDropdown} className="flex items-center cursor-pointer hover:text-coral-pink">
-               Events
-               <span className="pl-2">
-               <AiOutlineDown />
-               </span>
-               </button>
-               {showMobileEventsDropdown && (
-               <div className="relative m-4 flex flex-col text-left font-normal w-max">
-               <Link href="/events" onClick={handleMobileDropdownLinkClick} className='hover:text-coral-pink'>
-                    Overview
-               </Link>
-               <Link href="/events/dev-day" onClick={handleDropdownLinkClick} className='hover:text-coral-pink'>
-                    Dev Day
-               </Link>
-               <Link href="/events/cs-expo" onClick={handleDropdownLinkClick} className='hover:text-coral-pink'>
-                    CS Expo
-               </Link>
-               <Link href="/events/committees" onClick={handleDropdownLinkClick} className='hover:text-coral-pink'>
-                    Committees
-               </Link>
+               <div className="relative group">
+                    <button onClick={toggleMobileEventsDropdown} className="flex items-center cursor-pointer hover:text-coral-pink">
+                         Events
+                              <span className="pl-2">
+                                   <AiOutlineDown />
+                              </span>
+                    </button>
+
+                    {showMobileEventsDropdown && (
+                         <div className="relative m-4 flex flex-col text-left font-normal w-max">
+                              <Link href="/events" onClick={handleMobileDropdownLinkClick} className='hover:text-coral-pink'>
+                                   Overview
+                              </Link>
+
+                              <Link href="/events/dev-day" onClick={handleDropdownLinkClick} className='hover:text-coral-pink'>
+                                   Dev Day
+                              </Link>
+
+                              <Link href="/events/cs-expo" onClick={handleDropdownLinkClick} className='hover:text-coral-pink'>
+                                   CS Expo
+                              </Link>
+
+                              <Link href="/events/committees" onClick={handleDropdownLinkClick} className='hover:text-coral-pink'>
+                                   Committees
+                              </Link>
+                         </div>
+                    )}
                </div>
-               )}
-          </div>
      
-          <div className="relative group">
-               <button onClick={toggleMobileProjectsDropdown} className="flex items-center justify-center cursor-pointer hover:text-coral-pink">
-               Projects
-               <span className="pl-2">
-               <AiOutlineDown />
-               </span>
-               </button>
-               {showMobileProjectsDropdown && (
-               <div className="relative m-4 flex flex-col text-left font-normal w-max">
-               <Link href="/projects" onClick={handleMobileDropdownLinkClick} className='hover:text-coral-pink'>
-                    Overview
-               </Link>
-               <Link href="/projects/hall-of-fame" onClick={handleDropdownLinkClick} className='hover:text-coral-pink'>
-                    Hall of Fame
-               </Link>
-               <Link href="/projects/collections-of-projects" onClick={handleDropdownLinkClick} className='hover:text-coral-pink'>
-                    Collections of Projects
-               </Link>
+               <div className="relative group">
+                    <button onClick={toggleMobileProjectsDropdown} className="flex items-center justify-center cursor-pointer hover:text-coral-pink">
+                         Projects
+                              <span className="pl-2">
+                                   <AiOutlineDown />
+                              </span>
+                    </button>
+
+                    {showMobileProjectsDropdown && (
+                         <div className="relative m-4 flex flex-col text-left font-normal w-max">
+                              <Link href="/projects" onClick={handleMobileDropdownLinkClick} className='hover:text-coral-pink'>
+                                   Overview
+                              </Link>
+
+                              <Link href="/projects/hall-of-fame" onClick={handleDropdownLinkClick} className='hover:text-coral-pink'>
+                                   Hall of Fame
+                              </Link>
+
+                              <Link href="/projects/collections-of-projects" onClick={handleDropdownLinkClick} className='hover:text-coral-pink'>
+                                   Collections of Projects
+                              </Link>
+                         </div>
+                    )}
                </div>
-               )}
-          </div>
-          <Link href="/partners" className="cursor-pointer hover:text-coral-pink">
-               Partners
-          </Link>
+
+               <Link href="/partners" className="cursor-pointer hover:text-coral-pink">
+                    Partners
+               </Link>
           </div>
      );
      };
 
-return (
-     <header
-          className={`w-full fixed z-10 top-0 right-0 left-0 transition-all ${
-          isNavbarAtTop ? 'h-auto' : 'bg-eerie-black'
-          }`}
-     >
-          <nav className="w-full mx-auto flex justify-between items-center px-6 py-3 bg-transparent">
-          <Link href="/">
-               <Image
-               src={isNavbarAtTop ? '/cs-expo-logo-black.png' : '/cs-expo-logo-white.png'}
-               alt="logo"
-               width={117}
-               height={85}
-               className="object-contain"
-               />
-          </Link>
-          <div className="cursor-pointer ml-auto flex" onClick={toggleMobileMenu}>
-               {isNavbarAtTop ? (
-               defaultMenu()
-               ) : (
-               mobileMenuVisible ? (
-               <div className='text-coral-pink'><IoClose /></div>
-               ) : (
-               (!mobileMenuVisible && isNavbarAtTop) ? (defaultMenu()) : (
-                    <div className='text-coral-pink'><FaBars /></div>
-               )
-               )
-               )}
-          </div>
-          </nav>
-          {mobileMenuVisible && !isNavbarAtTop ? mobileMenu() : null}
-     </header>
+     return (
+          <header ref={mobileNavbarRef} className={`w-full fixed z-10 top-0 right-0 left-0 transition-all ${isNavbarAtTop ? 'h-auto' : 'bg-eerie-black'}`}>
+               <nav className="w-full mx-auto flex justify-between items-center px-6 py-3 bg-transparent">
+                    <Link href="/">
+                         <Image
+                              src={isNavbarAtTop ? '/cs-expo-logo-black.png' : '/cs-expo-logo-white.png'}
+                              alt="logo"
+                              width={117}
+                              height={85}
+                              className="object-contain"
+                         />
+                    </Link>
+
+                    <div className="cursor-pointer ml-auto flex" onClick={toggleMobileMenu}>
+                         {isNavbarAtTop ? (defaultMenu()) : (
+                              mobileMenuVisible ? (
+                                   <div className='text-coral-pink'><IoClose /></div>) : (
+                                        (!mobileMenuVisible && isNavbarAtTop) ? (<> {toggleMobileMenu()} {defaultMenu()} </>) : (
+                                        <div className='text-coral-pink'><FaBars /></div>)))
+                              }
+                    </div>
+               </nav>
+
+               {mobileMenuVisible && !isNavbarAtTop ? mobileMenu() : null}
+          </header>
      );
      };
      
