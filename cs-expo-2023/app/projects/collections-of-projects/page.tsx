7aa9550c9ffd66b8732d9bea3dca529e5936bbf9
Reggie Guday <coderej16@gmail.com>
2023-10-24 20:04:27 +0800

    some minor changes i guess

--- cs-expo-2023/app/projects/collections-of-projects/page.tsx
+++ cs-expo-2023/app/projects/collections-of-projects/page.tsx
@@ -7,7 +7,7 @@ import GroupsData from '../../../thesis-projects.json';
 
 const CollectionOfProjects: NextPage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col p-24">
       <CollectionOfProjectsHeader 
         headerText ="COLLECTION OF PROJECTS" 
         subheaderText="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic doloribus pariatur sapiente error iste, reiciendis, minima odit tenetur, rerum consequatur harum? Rerum molestiae fugit cupiditate pariatur corrupti, error quo quas."/>
--- cs-expo-2023/components/GroupCardLayout.tsx
+++ cs-expo-2023/components/GroupCardLayout.tsx
@@ -15,35 +15,37 @@ interface GroupCardProps {
 
 const GroupCard: React.FC<GroupCardProps> = ({ groupName, group }) => {
   return (
-    <div className="flex flex-col w-[300px] h-[400px] bg-stone-400 s-full justify-end">
-      <div className="flex s-full">
-        <p></p>
+    <div className="flex flex-col w-[300px] h-[450px] bg-stone-300 s-full justify-end">
+      <div className="flex ">
+
       </div>
-      <div className="flex h-[100px] bg-rose-500 s-full text-white text-start text-2xl font-bold font-['Helvetica Now Text ']">
-        <h1>{groupName}</h1>
+      <div className="flex flex-col h-[140px] bg-rose-500 s-full text-white text-start text-2xl px-2 pr-2 pt-1">
+        <div className="font-bold font-['Helvetica Now Text']">{groupName}</div>
+        <div className="text-sm font-['Helvetica Now Text']">{group.thesisTitle}</div>
       </div>
     </div>
   );
 }
 
 interface GroupCardLayoutProps {
-  groupData: Map<any,any>;
+  groupData: any;
 }
 const GroupCardLayout: React.FC<GroupCardLayoutProps> = ({ groupData }) => {
   const groupNames = Object.keys(groupData);
   const groupCards = [];
 
   groupNames.forEach((groupName, index) => {
-    // console.log(groupData[groupName]);
     groupCards.push(
       <GroupCard key={index} groupName={groupName} group={groupData[groupName]}/>
     )
   });
 
   return (
-    <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 content-evenly pt-20">
-      <>{groupCards}</>
-    </div>
+    <main>
+      <div className="grid 2xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 pt-20">
+        <>{groupCards}</>
+      </div>
+    </main>
   );
 }
 
