0cb21662d493e3ab9a53b5523c74a7c1de482bbc
Kyle Español <kylekce@gmail.com>
2023-10-25 02:40:08 +0800

    Update page.tsx
    Note: Please add the CDN links in the layout

--- cs-expo-2023/app/projects/page.tsx
+++ cs-expo-2023/app/projects/page.tsx
@@ -4,108 +4,80 @@
 import React, { useEffect } from "react";
 import Link from "next/link";
 import { AiOutlineRight } from "react-icons/ai";
+import ProjectsHeader from "@/components/ProjectsHeader";
+import ThesisPoster from "@/components/ThesisPosterCard";
 
 const Projects = () => {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const ScrollCarousel = require("scroll-carousel");
-      new ScrollCarousel(".projects-overview-carousel", {
-        speed: 2, // Adjust the speed of the carousel
-        smartSpeed: false,
-        autoplay: true,
-      });
-    }
-  }, []);
+    useEffect(() => {
+        new ScrollCarousel(".my-carousel", {
+            autoplay: true,
+        });
+    }, []);
 
-  // Define the categories and the number of thesis titles for each
-  const categories = [
-    { name: "Internet of Things", count: 3 },
-    { name: "Education", count: 5 },
-    { name: "Natural Language Processing", count: 3 },
-    { name: "Image Processing and Computer Vision", count: 3 },
-    { name: "Data Analytics", count: 3 },
-    { name: "Health", count: 3 },
-  ];
+    // Define the categories and the number of thesis titles for each
+    const categories = [
+        { name: "Internet of Things", count: 3 },
+        { name: "Education", count: 5 },
+        { name: "Natural Language Processing", count: 3 },
+        { name: "Image Processing and Computer Vision", count: 3 },
+        { name: "Data Analytics", count: 3 },
+        { name: "Health", count: 3 },
+    ];
 
-  // Create an array to store all the placeholders for thesis posters
-  const thesisTitles = Array.from(
-    { length: 20 },
-    (_, index) => `Thesis Title ${index + 1}`
-  );
+    // Create an array to store all the placeholders for thesis posters
+    const thesisTitles = Array.from(
+        { length: 20 },
+        (_, index) => `Thesis Title ${index + 1}`
+    );
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* The title and description of the page */}
-      <div className="text-3xl font-bold text-center text-zinc-900 font-['Helvetica Now Text ']">
-        PROJECTS
-      </div>
-      <div className="text-xs text-center text-zinc-900 font-['Helvetica Now Text ']">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        <br />
-        tempor incididunt ut labore et dolore magna aliqua. Nisl purus in mollis
-        <br />
-        nunc sed id semper risus. Volutpat consequat mauris nunc congue nisi
-        <br />
-        vitae suscipit tellus.
-      </div>
-
-      <br />
-
-      {/* The links to the Hall of Fame and Collections of Projects pages */}
-      <div className="flex space-x-4">
-        <Link
-          href="/projects/hall-of-fame"
-          className="hall text-xs text-center text-rose-500 font-bold font-['Helvetica Now Text ']"
-        >
-          Hall of Fame
-          <AiOutlineRight className="inline-block" />
-        </Link>
-
-        <Link
-          href="/projects/collections-of-projects"
-          className="collections text-xs text-center text-rose-500 font-bold font-['Helvetica Now Text ']"
-        >
-          Collections of Projects <AiOutlineRight className="inline-block" />
-        </Link>
-      </div>
-
-      <br />
-
-      <link
-        // The CDN (Content Delivery Network) link for the scroll-carousel stylesheet
-        // https://asif-jalil.github.io/scroll-carousel-website/
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/scroll-carousel@1.2.7/dist/scroll.carousel.min.css"
-      />
-
-      {/* The carousel that displays the thesis posters */}
-      <div className="projects-overview-carousel">
-        {categories.map((category, categoryIndex) => (
-          <div key={categoryIndex} className="category-box">
-            <h2>{category.name}</h2>
-            <div className="category-posters">
-              {thesisTitles
-                .slice(0, category.count) // Take the specified number of posters for the category
-                .map((title, posterIndex) => (
-                  <div
-                    key={posterIndex}
-                    className="category-poster"
-                    style={{
-                      backgroundColor: posterIndex % 2 === 0 ? "red" : "black",
-                      color: "white",
-                      height: "300px",
-                      width: "300px",
-                    }}
-                  >
-                    {title}
-                  </div>
-                ))}
+    return (
+        <main className="flex flex-col items-center justify-between p-24">
+            <ProjectsHeader headerText="PROJECTS" subheaderText="LOREM" />
+            <div className="flex justify-center p-4 w-full">
+                <Link
+                    href="/projects/hall-of-fame"
+                    className="flex items-center cursor-pointer ml-4 font-medium text-coral-pink"
+                >
+                    Hall of Fame{" "}
+                    <span className="flex-auto">
+                        <AiOutlineRight />
+                    </span>
+                </Link>
+                <Link
+                    href="/projects/collections-of-projects"
+                    className="flex items-center cursor-pointer ml-4 font-medium text-coral-pink"
+                >
+                    Collections of Projects{" "}
+                    <span>
+                        <AiOutlineRight />
+                    </span>
+                </Link>
             </div>
-          </div>
-        ))}
-      </div>
-    </main>
-  ); // End of return
-}; // End of Projects
+
+            <div className="w-[1400px]">
+                <div className="my-carousel">
+                    <div className="my-slide w-[300px] h-[400px] bg-slate-100">
+                        1
+                    </div>
+                    <div className="my-slide w-[300px] h-[400px] bg-slate-100">
+                        2
+                    </div>
+                    <div className="my-slide w-[300px] h-[400px] bg-slate-100">
+                        3
+                    </div>
+                    <div className="my-slide w-[300px] h-[400px] bg-slate-100">
+                        4
+                    </div>
+                    <div className="my-slide w-[300px] h-[400px] bg-slate-100">
+                        5
+                    </div>
+                    <div className="my-slide w-[300px] h-[400px] bg-slate-100">
+                        6
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+};
 
 export default Projects;
