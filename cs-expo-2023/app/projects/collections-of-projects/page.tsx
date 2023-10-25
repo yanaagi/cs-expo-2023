b1471a9da08ed81aefd5aaca1d9b41535deeebfa
LimyuA <92149293+LimyuA@users.noreply.github.com>
2023-10-25 16:48:55 +0800

    fix: FIX collections-of-projects/page.tsx not running and MODIFY color for component GroupCardLayout.tsx

--- cs-expo-2023/app/projects/collections-of-projects/page.tsx
+++ cs-expo-2023/app/projects/collections-of-projects/page.tsx
@@ -38,12 +38,6 @@ const CollectionOfProjects: NextPage = () => {
         subheaderText="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic doloribus pariatur sapiente error iste, reiciendis, minima odit tenetur, rerum consequatur harum? Rerum molestiae fugit cupiditate pariatur corrupti, error quo quas."/>
 
       <GroupCardLayout groupData={GroupsData}/>
-    <main className="flex min-h-screen flex-col p-24">
-      <CollectionOfProjectsHeader 
-        headerText ="COLLECTION OF PROJECTS" 
-        subheaderText="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic doloribus pariatur sapiente error iste, reiciendis, minima odit tenetur, rerum consequatur harum? Rerum molestiae fugit cupiditate pariatur corrupti, error quo quas."/>
-
-      <GroupCardLayout groupData={GroupsData}/>
     </main>
   )
 }
