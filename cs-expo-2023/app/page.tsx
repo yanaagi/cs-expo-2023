9663b9c23e4f346bd53ff73463e0f9d06136e15a
Reggie Guday <coderej16@gmail.com>
2023-10-25 16:11:54 +0800

    Update page.tsx

--- cs-expo-2023/app/page.tsx
+++ cs-expo-2023/app/page.tsx
@@ -5,10 +5,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 place-items-center">
       <h1 className="font-black place-items-center p-8">Paving The New Technospace Through Paradigm Shifting Innovations</h1>
-
-      <Routes>
-        
-      </Routes>
     </main>
   )
 }
