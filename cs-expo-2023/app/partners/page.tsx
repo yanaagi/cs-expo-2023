24a5af5a451c6005a97ea6c9960cc684123cbe2d
Dale0122 <asdale0122@gmail.com>
2023-11-08 09:07:34 +0800

    feat: FIX alternating layout

--- cs-expo-2023/app/partners/page.tsx
+++ cs-expo-2023/app/partners/page.tsx
@@ -17,17 +17,18 @@ const Partners: NextPage = () => {
         <div className="flex items-center justify-center p-24">
           <PartnerIconsLayout partners={partnerArray} />
         </div>
+        <h2 id="major" className="font-black text-9xl m-0 mt-[-1rem] flex justify-center p-24">MAJOR PARTNERS</h2>
         <div>
           <AlternatingPartnersLayout partners={partnerArray} />
         </div>
         <div>
-          <h2 className="font-black text-9xl m-0 mt-[-1rem] flex justify-center p-24">MINOR PARTNERS</h2>
+          <h2 id="minor" className="font-black text-9xl m-0 mt-[-1rem] flex justify-center p-24">MINOR PARTNERS</h2>
           <div>
             <AlternatingPartnersLayout partners={partnerArray} />
           </div>
         </div>
         <div>
-          <h2 className="font-black text-9xl m-0 mt-[-1rem] flex justify-center p-24">MEDIA PARTNERS</h2>
+          <h2 id="media" className="font-black text-9xl m-0 mt-[-1rem] flex justify-center p-24">MEDIA PARTNERS</h2>
           <div>
             <AlternatingPartnersLayout partners={partnerArray} />
           </div>
