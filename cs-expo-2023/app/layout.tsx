555292f37e9733b6f5de3a386504bf15eea7da11
kylekce <kylekce@gmail.com>
2023-11-10 22:07:34 +0800

    Revert the layout

--- cs-expo-2023/app/layout.tsx
+++ cs-expo-2023/app/layout.tsx
@@ -22,35 +22,13 @@ export default function RootLayout({
                     href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
                 />
 
-                {/* GlideJS's two CDN (both required) */}
+                {/* GlideJS */}
                 <link
                     rel="stylesheet"
-                    href="https://cdn.jsdelivr.net/npm/@glidejs/glide/dist/css/glide.core.min.css"
+                    href="node_modules/@glidejs/glide/dist/css/glide.core.min.css"
                 />
 
-                <link
-                    rel="stylesheet"
-                    href="https://cdn.jsdelivr.net/npm/@glidejs/glide/dist/css/glide.theme.min.css"
-                />
-
-                <script
-                    src="https://cdn.jsdelivr.net/npm/@glidejs/glide"
-                    async
-                ></script>
-
-                {/* Scroll Carousel's CDN */}
-                <link
-                    rel="stylesheet"
-                    href="https://cdn.jsdelivr.net/npm/scroll-carousel@1.2.7/dist/scroll.carousel.min.css"
-                />
-
-                <script
-                    src="https://cdn.jsdelivr.net/npm/scroll-carousel@1.2.7/dist/scroll.carousel.min.js"
-                    async
-                ></script>
-
-                {/* SplideJS */}
-                <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js" />
+                <script src="https://cdn.jsdelivr.net/npm/@glidejs/glide"></script>
             </head>
             <body>
                 <NavBar />
