e38b1529719465bd9cee345ad76a727dd836e180
patagbayani <nicolaeagbayani@gmail.com>
2023-11-12 19:33:46 +0800

    CHANGE the squares and header are now responsive and the squares have functionalities and responsive

--- cs-expo-2023/components/MeetTheCommitteesHeader.tsx
+++ cs-expo-2023/components/MeetTheCommitteesHeader.tsx
@@ -10,10 +10,10 @@ const MeetTheCommitteesHeader: React.FC<MeetTheCommitteesProps> = ({ headerText,
         <main>
             <div className="p-23">
                 <div className="ms-28">
-                    <div className="font-regular text-7xl m-0 mb-[-1.5rem]">
+                    <div className="font-regular text-6xl md:text-6xl lg:text-6xl xl:text-7xl m-0 mb-[-1.5rem]">
                         <p>{headerText}</p>
                     </div>
-                    <div className="font-black text-8xl m-0 mt-[-1rem]">
+                    <div className="font-black text-6xl md:text-7xl lg:text-7xl xl:text-8xl m-0 mt-[1rem]">
                         <p>{subheaderText}</p>
                     </div>
                 </div>
