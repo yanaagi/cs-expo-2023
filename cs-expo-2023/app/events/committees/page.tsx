7faf3b62ca7b2440b885eea414490921f01d8d96
rlme17 <richespino17@gmail.com>
2023-11-24 21:04:32 +0800

    feat: ADD mames for the committee members

--- cs-expo-2023/app/events/committees/page.tsx
+++ cs-expo-2023/app/events/committees/page.tsx
@@ -13,8 +13,9 @@ const EventsCommittees = () => {
         />
 
 <CommitteesGrid/>
+<div id="PROGRAMS COMMITTEE">
 <CommitteesPic 
-          committeeName='PROGRAMS'
+          committeeName='PROGRAMS COMMITTEE'
           committeePic={[
             '/group-images/Quinta/10.jpg',
             '/group-images/Codebebs/10.jpg',
@@ -26,13 +27,25 @@ const EventsCommittees = () => {
             '/group-images/Teasis/5.jpg',
             '/group-images/Hello World/4.jpg',
             '/group-images/Hello World/new11.jpg',
-
-
+          ]}
+          memberName={[
+            'Michaella Gonzales',
+            'Noriel Joy Embudo',
+            'Art Eli Bautista',
+            'John Dominic Contreras',
+            'Francis Nino Santos',
+            'Rico Ray Alido',
+            'Andre Martee Valerio',
+            'Kenneth Bryce Lim',
+            'Allan Jr. Magtibay',
+            'Randel Jason Espiritu',
           ]}
           />
+</div>
 
+<div id="SECRETARIAT COMMITTEE">
         <CommitteesPic 
-          committeeName='SECRETARIAT AND AWARDS COMMITTEE'
+          committeeName='SECRETARIAT COMMITTEE'
           committeePic={[
             '/group-images/Adorables/15.jpg',
             '/group-images/Quinta/20.jpg',
@@ -42,25 +55,43 @@ const EventsCommittees = () => {
             '/group-images/Adorables/7.jpg',
             '/group-images/Engeenz/11.jpg',
             '/group-images/Engeenz/9.jpg',
+            '/group-images/Engeenz/.jpg',
             '/group-images/Midnight Coders/12.jpg',
-            
-
-
+          ]}
+          memberName={[
+            'Erica Francia Evangelista',
+            'Maria Evangelicalyn Ong',
+            'Alkier Gabriel Magallanes',
+            'Varon James Medenilla',
+            'Gerard Gueco',
+            'Clintt Adora',
+            'Gerardo Joshua Dela Cruz',
+            'Curt Russel Celeste',
+            'Vernon Gabriel Angelo Vicencio',
+            'Phoebe Kelly Liwanag',
           ]}
           />
+</div>
+<div id="SPONSORSHIP & PARTNERSHIP COMMITTEE">
         <CommitteesPic 
-          committeeName='SPONSORSHIP AND PARTNERSHIP'
+          committeeName='SPONSORSHIP & PARTNERSHIP COMMITTEE'
           committeePic={[
             '/group-images/Array Co/16.jpg',
             '/group-images/Array Co/20.jpg',
             '/group-images/Softyware/14.jpg',
             '/group-images/Softyware/',
-
+          ]}
+          memberName={[
+            'Bianca Goldman',
+            'John Angelo Yap',
+            'Drennix Jean-Roe Guerrero',
+            'Daniel Marco Rafanan',
           ]}
           />
-
+</div>
+<div id="SPEAKERS & PANELIST COMMITTEE">
 <CommitteesPic 
-          committeeName='SPEAKERS AND PANELIST'
+          committeeName='SPEAKERS & PANELIST COMMITTEE'
           committeePic={[
             '/group-images/Deos/5.jpg',
             '/group-images/Deos/9.jpg',
@@ -68,46 +99,18 @@ const EventsCommittees = () => {
             '/group-images/-/-',
             '/group-images/-/-',
           ]}
-          />
-
-<CommitteesPic 
-          committeeName='CREATIVES AND MEDIA'
-          committeePic={[
-            '/group-images/Deos/18.jpg',
-            '/group-images/Innov8/9.jpg',
-            '/group-images/Hello World/9.jpg',
-            '/group-images/Teasis/12.jpg',
-            '/group-images/Teasis/9.jpg',
-            '/group-images/Peaky Blinders/9.jpg',
-            '/group-images/Engeenz/13.jpg',
-            '/group-images/Peaky Blinders/ne11.jpg',
-            '/group-images/Quinta/new17.jpg',
-            '/group-images/Stark/16.jpg',
-
-
+          memberName={[
+            'Wilem Joshua Offemaria',
+            'Christian Dale Selisana',
+            'Cyril John Altares',
+            'John Jordan Espiritu',
+            'Elijah Edgard Velasco',
           ]}
           />
-
-          <CommitteesPic 
-          committeeName='PUBLICITY AND PUBLICATIONS'
-          committeePic={[
-            '/group-images/Rob_lux/8.jpg',
-            '/group-images/Rob_lux/6.jpg',
-            '/group-images/Rob_lux/14.jpg',
-            '/group-images/Softyware/8.jpg',
-            '/group-images/Peaky Blinders/13.jpg',
-            '/group-images/Yaymiko/10.jpg',
-            '/group-images/Yaymiko/9.jpg',
-            '/group-images/Yaymiko/5.jpg',
-            '/group-images/Quinta/21.jpg',
-            '/group-images/Innov8/19.jpg',
-
-
-          ]}
-          />
-
+</div>
+<div id="LOGISTICS COMMITTEE">
 <CommitteesPic 
-          committeeName='LOGISTICS AND FACILITIES'
+          committeeName='LOGISTICS COMMITTEE' 
           committeePic={[
             '/group-images/Innov8/6.jpg',
             '/group-images/Midnight Coders/13.jpg',
@@ -135,10 +138,96 @@ const EventsCommittees = () => {
             '/group-images/Codebebs/16.jpg',
             '/group-images/Rob_lux/13.jpg',
           ]}
+          memberName={[
+            'Rhuzzel Paramio',
+            'Mark Wilson Bucsit',
+            'Micko Alhwin Andrada',
+            'Franco Gian Ramos',
+            'Kyle Austin Dimabasa',
+            'Juan Alfonso Magpantay',
+            'Jan Miguel Pinlac',
+            'Kyle Rainiel Baltazar',
+            'Jewel Anne Panela',
+            'Gabrielle Rose Gacula',
+            'Patrick Wayne Tantay',
+            'Kyle Dominic Vallo',
+            'Seth Johann Esteban',
+            'Gavino Othello Angeles',
+            'Daniel Joseph Alvarez',
+            'Michael John Rola',
+            'John Onel Bulos',
+            'Alijah Ethan Mazo',
+            'Yunno Honesto Rico Belleza',
+            'Jed Marco Mendizabal',
+            'Carlo Eraño Cruz', 
+            'Fhiel Nichole Ashley Nanalig',
+            'Zedric James Ramoso',
+            'Paul Andre Gabriel',
+            'Brylle Miguel Andres',
+          ]}
           />
-
+</div>
+<div id="PUBLICITY & PUBLICATION COMMITTEE">
+          <CommitteesPic 
+          committeeName='PUBLICITY & PUBLICATIONS COMMITTEE'
+          committeePic={[
+            '/group-images/Rob_lux/8.jpg',
+            '/group-images/Rob_lux/6.jpg',
+            '/group-images/Rob_lux/14.jpg',
+            '/group-images/Softyware/8.jpg',
+            '/group-images/Peaky Blinders/13.jpg',
+            '/group-images/Yaymiko/10.jpg',
+            '/group-images/Yaymiko/9.jpg',
+            '/group-images/Yaymiko/5.jpg',
+            '/group-images/Quinta/21.jpg',
+            '/group-images/Innov8/19.jpg',
+          ]}
+          memberName={[
+            'Jose Ian Miguel De Leon',
+            'Leigh Reign Ascaño', 
+            'Mark Revin Fragata',
+            'Jeremie Calinisan',
+            'Dioshua Sapnu',
+            'Ivan Dominic Albano',
+            'Kyle Sebastian Claveria',
+            'Charles Dmitri Peñaranda', 
+            'Charles Christian Sapida',
+            'Cyrill James Dela Cruz',
+          ]}
+          />
+</div>
+<div id="CREATIVES & MEDIA COMMITTEE">
 <CommitteesPic 
-          committeeName='DEVELOPERS'
+          committeeName='CREATIVES & MEDIA COMMITTEE'
+          committeePic={[
+            '/group-images/Deos/18.jpg',
+            '/group-images/Innov8/9.jpg',
+            '/group-images/Hello World/9.jpg',
+            '/group-images/Teasis/12.jpg',
+            '/group-images/Teasis/9.jpg',
+            '/group-images/Peaky Blinders/9.jpg',
+            '/group-images/Engeenz/13.jpg',
+            '/group-images/Peaky Blinders/ne11.jpg',
+            '/group-images/Quinta/new17.jpg',
+            '/group-images/Stark/16.jpg',
+          ]}
+          memberName={[
+            'John Felix Agda',
+            'Jude Michael Francia',
+            'Carl Anthony Lar Carandan',
+            'Edra Abel Gomez',
+            'Joshua Azarcon',
+            'John Florenz Reyes',
+            'Miguel Angelo Fesalbon',
+            'Marvin Simsiman',
+            'Dhancovic Ivan Mendoza',
+            'Sally Jr. Segundo',
+          ]}
+          />
+</div>
+<div id="DEVELOPERS COMMITTEE">
+<CommitteesPic 
+          committeeName='DEVELOPERS COMMITTEE'
           committeePic={[
             '/group-images/Array Co/9.jpg',
             '/group-images/Javangers/13.jpg',
@@ -155,12 +244,27 @@ const EventsCommittees = () => {
             '/group-images/Array Co/14.jpg',
             '/group-images/Stark/12.jpg',
             '/group-images/Stark/9.jpg',
-
-
+          ]}
+          memberName={[
+            'Jerico John Almoro',
+            'Alan Gabriel Limyu',
+            'Nichole Anne Avañez', 
+            'Ralph Henrik Cajipe',
+            'Reggie Noel Guday',
+            'Vence Carl Esquivel',
+            'Lorenzo Miguel Dela Cruz',
+            'Richard Lyndon Espino',
+            'Jeri-Michael Joseph Advincula',
+            'Zeno Gilrence Donate',
+            'David John Bas',
+            'Patrick Nicolae Agbayani',
+            'Francis Dale Cañon', 
+            'Kyle Christian Español', 
+            'Charles Andrew Santos',
           ]}
           />
       </div>
-      
+      </div>
     </main>
   );
 };
--- cs-expo-2023/components/CommitteesPic.tsx
+++ cs-expo-2023/components/CommitteesPic.tsx
@@ -4,34 +4,36 @@ import Image from 'next/image';
 interface CommitteesPicProps {
   committeeName: string;
   committeePic?: string[];
+  memberName?: string[];
 }
 
-const EventsCommittees: React.FC<CommitteesPicProps> = ({committeeName, committeePic}) => {
+const EventsCommittees: React.FC<CommitteesPicProps> = ({committeeName, committeePic, memberName}) => {
   return (
   <main className="flex min-h-screen flex-col px-10 -mx-10 pt-10">
   
    {/* WEB VIEW */}
    <div className="mt-5 justify-center hidden lg:block">
-        <div className="grid grid-cols-1">
-          <div className="col-span-1 custom-font text-center text-5xl">
-            {committeeName}
-          </div>
-        </div>
-        <div className="flex justify-center flex-wrap -mx-10 my-10"> {/* Decreased margin */}
-          {committeePic && committeePic.map((path, index) => (
-            <div key={index} className="w-1/4 p-10 md:w-1/5"> {/* Changed width to 1/4 and 1/5 for different breakpoints */}
-              <div className="h-64 w-44 p-4 bg-timberwolf relative">
-                <Image
-                  src={path}
-                  alt={`${committeeName} Image ${index + 1}`}
-                  fill
-                  className="w-full h-auto mb-2"
-                />
-              </div>
-            </div>
-          ))}
-        </div>
+    <div className="grid grid-cols-1">
+      <div className="col-span-1 custom-font text-center text-5xl">
+        {committeeName}
       </div>
+    </div>
+    <div className="flex justify-center flex-wrap -mx-10 my-10"> {/* Decreased margin */}
+      {committeePic && committeePic.map((path, index) => (
+        <div key={index} className="w-1/4 md:w-1/5 p-10 flex flex-col items-center"> {/* Adjusted width and added flex styles */}
+          <div className="h-64 w-44 p-4 bg-timberwolf relative flex items-center justify-center"> {/* Centering image */}
+            <Image
+              src={path}
+              alt={`${committeeName} Image ${index + 1}`}
+              fill
+              className="w-full h-auto mb-2"
+            />
+          </div>
+          <h1 className='text-center mt-2 custom-font'>{memberName && memberName[index]}</h1> {/* Adjusted margin top */}
+        </div>
+      ))}
+    </div>
+  </div>
 
 
     
@@ -43,8 +45,8 @@ const EventsCommittees: React.FC<CommitteesPicProps> = ({committeeName, committe
   </div>
   <div className="flex justify-center flex-wrap -mx-10 my-10">
     {committeePic && committeePic.map((path, index) => (
-      <div key={index} className="w-1/3 p-10">
-        <div className="h-64 w-44 p-4 bg-timberwolf relative">
+      <div key={index} className="w-1/3 p-10 flex flex-col items-center">
+        <div className="h-64 w-44 p-4 bg-timberwolf relative flex items-center justify-center">
           <Image
             src={path}
             alt={`${committeeName} Image ${index + 1}`}
@@ -52,6 +54,7 @@ const EventsCommittees: React.FC<CommitteesPicProps> = ({committeeName, committe
             className="w-full h-auto mb-2"
           />
         </div>
+        <h1 className='text-center mt-2 custom-font'>{memberName && memberName[index]}</h1> {/* Adjusted margin top */}
       </div>
     ))}
   </div>
@@ -66,8 +69,8 @@ const EventsCommittees: React.FC<CommitteesPicProps> = ({committeeName, committe
   </div>
   <div className="flex justify-center flex-wrap -mx-10 my-10">
     {committeePic && committeePic.map((path, index) => (
-      <div key={index} className="w-1/1 p-10">
-        <div className="h-64 w-44 p-4 bg-timberwolf relative">
+      <div key={index} className="w-1/1 p-10 flex flex-col items-center">
+        <div className="h-64 w-44 p-4 bg-timberwolf relative flex items-center justify-center">
           <Image
             src={path}
             alt={`${committeeName} Image ${index + 1}`}
@@ -75,6 +78,7 @@ const EventsCommittees: React.FC<CommitteesPicProps> = ({committeeName, committe
             className="w-full h-auto mb-2"
           />
         </div>
+        <h1 className='text-center mt-2 custom-font'>{memberName && memberName[index]}</h1> {/* Adjusted margin top */}
       </div>
     ))}
   </div>
