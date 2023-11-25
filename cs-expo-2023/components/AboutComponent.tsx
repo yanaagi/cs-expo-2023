8d62010b3b46c135eae0ae85a3d3d9bb87de02c6
Zeno Donato <zeno16.donato@gmail.com>
2023-11-25 22:02:34 +0800

    feat: dev-day responsiveness

--- cs-expo-2023/components/AboutComponent.tsx
+++ cs-expo-2023/components/AboutComponent.tsx
@@ -14,8 +14,8 @@ const AboutComponent: React.FC<AboutComponentProps> = ({ customText, sampleText 
           {customText}
         </p>
       </div>
-      <div className="p-1 w-3/4 bg-coral-pink mt-3 mb-12">
-        <img src="/dev-day/dev-day04.jpg"/>
+      <div className="p-1.5 text-center font-medium bg-coral-pink w-auto sm:w-2/3 2xl:w-full sm:h-24 md:h-32 lg:h-48 xl:h-60 2xl:h-full mt-1 sm:mt-2 md:mt-3 lg:mt-4 xl:mt-5 2xl:mt-6 mb-5 sm:mb-5 md:mb-5 lg:mb-6 xl:mb-8 2xl:mb-10 ml-5 sm:ml-6 md:ml-20 lg:ml-28 xl:ml-36 2xl:ml-40 mr-1 sm:mr-2 md:mr-3 lg:mr-4 xl:mr-14 2xl:mr-10 relative">
+        <img src="/dev-day/dev-day04.jpg " className="object-cover w-full h-full" />
       </div>
     </div>
   );
