5b2100c1356e2e19576d5bfc8b61515005e280bc
kylekce <kylekce@gmail.com>
2023-11-30 18:50:20 +0800

    Update the contents

--- cs-expo-2023/app/projects/hall-of-fame/page.tsx
+++ cs-expo-2023/app/projects/hall-of-fame/page.tsx
@@ -4,7 +4,7 @@ import Carousel from "@/components/HallOfFameCarousel";
 
 const HallOfFame = () => {
     return (
-        <main className="pt-24 sm:flex sm:flex-col sm:items-center">
+        <main className="mt-28 sm:flex sm:flex-col sm:items-center">
             <ProjectsHeader
                 headerText="HALL OF FAME"
                 subheaderText="
@@ -13,74 +13,344 @@ const HallOfFame = () => {
 
             {/* Overall Best Thesis */}
             <Carousel
-                title="OVERALL BEST THESIS" // Title of the carousel
-                // Don't forget to change the type of the slides in the Carousel component when changing it to photos or videos
-                // Slide details: ranking(1st Place, etc.), group name, thesis title, link of the group's page
+                title="BEST THESIS OVERALL"
+                titleSize="1.5rem"
                 slides={[
                     {
-                        backgroundColor: "var(--timberwolf)",
+                        backgroundImage:
+                            "/hall-of-fame/best-thesis-overall/1st-place.jpg",
                         ranking: "1st Place",
-                        group: "Lorem",
-                        thesis: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-                        link: "/",
+                        group: "Array Co.",
+                        thesis: "DASH: Composite Restoration Using Image Recognition for Teeth Shade Matching Using Deep Learning",
+                        link: "/projects/Array-Co",
                     },
                     {
-                        backgroundColor: "var(--timberwolf)",
+                        backgroundImage:
+                            "/hall-of-fame/best-thesis-overall/2nd-place.jpg",
                         ranking: "2nd Place",
-                        group: "Lorem",
-                        thesis: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-                        link: "/",
+                        group: "STARK",
+                        thesis: "VIDEO LENS: Video Content Analysis through Computer Vision and Natural Language Processing",
+                        link: "/projects/S.T.A.R.K.",
                     },
                     {
-                        backgroundColor: "var(--timberwolf)",
+                        backgroundImage:
+                            "/hall-of-fame/best-thesis-overall/3rd-place.jpg",
                         ranking: "3rd Place",
-                        group: "Lorem",
-                        thesis: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-                        link: "/",
+                        group: "TeaSis",
+                        thesis: "GameFace: Head Tracking and Facial Recognition and Detection as Inputs for Gaming using Haar Cascade and MediaPipe Framework",
+                        link: "/projects/TeaSis",
                     },
-                ]}  
-                id="carousel-overall-best-thesis" // ID of the carousel
-                carouselNumber={1} // Page number of the carousel
-                carouselUp="hidden" // Hide/Show the up arrow (options: "hidden", "")
-                carouselDown="" // Hide/Show the down arrow (options: "hidden", "")
-                carouselUpTarget="" // ID of the carousel to go to when clicking the up arrow
-                carouselDownTarget="carousel-innovative-award" // ID of the carousel to go to when clicking the down arrow
+                ]}
+                id="carousel-best-thesis-overall"
+                carouselNumber={1}
+                carouselUp="hidden"
+                carouselDown=""
+                carouselUpTarget=""
+                carouselDownTarget="carousel-most-innovative-study"
+                carouselLeft=""
+                carouselRight=""
             />
 
-            {/* Innovative Award */}
             <Carousel
-                title="INNOVATIVE AWARD" // Title of the carousel
-                // Don't forget to change the type of the slides in the Carousel component when changing it to photos or videos
-                // Slide details: ranking(1st Place, etc.), group name, thesis title, link of the group's page
+                title="MOST INNOVATIVE STUDY"
+                titleSize="1.3rem"
                 slides={[
                     {
-                        backgroundColor: "var(--timberwolf)",
+                        backgroundImage:
+                            "/hall-of-fame/most-innovative-study/1st-place.jpg",
                         ranking: "1st Place",
-                        group: "Lorem",
-                        thesis: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-                        link: "/",
+                        group: "STARK",
+                        thesis: "VIDEO LENS: Video Content Analysis through Computer Vision and Natural Language Processing",
+                        link: "/projects/S.T.A.R.K.",
                     },
                     {
-                        backgroundColor: "var(--timberwolf)",
+                        backgroundImage:
+                            "/hall-of-fame/most-innovative-study/2nd-place.jpg",
                         ranking: "2nd Place",
-                        group: "Lorem",
-                        thesis: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-                        link: "/",
+                        group: "Array Co.",
+                        thesis: "DASH: Composite Restoration Using Image Recognition for Teeth Shade Matching Using Deep Learning",
+                        link: "/projects/Array-Co",
                     },
                     {
-                        backgroundColor: "var(--timberwolf)",
+                        backgroundImage:
+                            "/hall-of-fame/most-innovative-study/3rd-place.jpg",
                         ranking: "3rd Place",
-                        group: "Lorem",
-                        thesis: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-                        link: "/",
+                        group: "Codebebs",
+                        thesis: "SirenaTech: A Water Quality Testing Algorithm for Detecting the Potability of Water",
+                        link: "/projects/Codebebs",
                     },
-                ]}  
-                id="carousel-innovative-award" // ID of the carousel
-                carouselNumber={2} // Page number of the carousel
-                carouselUp="" // Hide/Show the up arrow (options: "hidden", "")
-                carouselDown="" // Hide/Show the down arrow (options: "hidden", "")
-                carouselUpTarget="carousel-overall-best-thesis" // ID of the carousel to go to when clicking the up arrow
-                carouselDownTarget="" // ID of the carousel to go to when clicking the down arrow
+                ]}
+                id="carousel-most-innovative-study"
+                carouselNumber={2}
+                carouselUp=""
+                carouselDown=""
+                carouselUpTarget="carousel-best-thesis-overall"
+                carouselDownTarget="carousel-best-presenter"
+                carouselLeft=""
+                carouselRight=""
+            />
+
+            <Carousel
+                title="BEST PRESENTER"
+                titleSize="1.5rem"
+                slides={[
+                    {
+                        backgroundImage:
+                            "/hall-of-fame/best-presenter/1st-place.jpg",
+                        ranking: "1st Place",
+                        group: "Abel Gomez (TeaSis)",
+                        thesis: "GameFace: Head Tracking and Facial Recognition and Detection as Inputs for Gaming using Haar Cascade and MediaPipe Framework",
+                        link: "/projects/TeaSis",
+                    },
+                    {
+                        backgroundImage:
+                            "/hall-of-fame/best-presenter/2nd-place.jpg",
+                        ranking: "2nd Place",
+                        group: "Ralph Cajipe (STARK)",
+                        thesis: "VIDEO LENS: Video Content Analysis through Computer Vision and Natural Language Processing",
+                        link: "/projects/S.T.A.R.K.",
+                    },
+                    {
+                        backgroundImage:
+                            "/hall-of-fame/best-presenter/3rd-place.jpg",
+                        ranking: "3rd Place",
+                        group: "Franco Gian Ramos (AiM)",
+                        thesis: "DEEP-COLON: Colorectal Disease Detection Using You Only Look Once Algorithm",
+                        link: "/projects/AiM",
+                    },
+                ]}
+                id="carousel-best-presenter"
+                carouselNumber={3}
+                carouselUp=""
+                carouselDown=""
+                carouselUpTarget="carousel-most-innovative-study"
+                carouselDownTarget="carousel-best-thesis-nlp"
+                carouselLeft=""
+                carouselRight=""
+            />
+
+            <Carousel
+                title="BEST THESIS IN NLP"
+                titleSize="1.5rem"
+                slides={[
+                    {
+                        backgroundImage:
+                            "/hall-of-fame/best-thesis-category/best-thesis-nlp.jpg",
+                        ranking: "",
+                        group: "STARK",
+                        thesis: "VIDEO LENS: Video Content Analysis through Computer Vision and Natural Language Processing",
+                        link: "/projects/S.T.A.R.K.",
+                    },
+                ]}
+                id="carousel-best-thesis-nlp"
+                carouselNumber={4}
+                carouselUp=""
+                carouselDown=""
+                carouselUpTarget="carousel-best-presenter"
+                carouselDownTarget="carousel-best-thesis-iot"
+                carouselLeft="hidden"
+                carouselRight="hidden"
+            />
+
+            <Carousel
+                title="BEST THESIS IN IOT"
+                titleSize="1.5rem"
+                slides={[
+                    {
+                        backgroundImage:
+                            "/hall-of-fame/best-thesis-category/best-thesis-iot.jpg",
+                        ranking: "",
+                        group: "INNOV8",
+                        thesis: "AirZen: Indoor Air Quality Monitoring and Recommendation System",
+                        link: "/projects/INNOV8",
+                    },
+                ]}
+                id="carousel-best-thesis-iot"
+                carouselNumber={5}
+                carouselUp=""
+                carouselDown=""
+                carouselUpTarget="carousel-best-thesis-nlp"
+                carouselDownTarget="carousel-best-thesis-image-processing"
+                carouselLeft="hidden"
+                carouselRight="hidden"
+            />
+
+            <Carousel
+                title="BEST THESIS IN IMAGE PROCESSING"
+                titleSize="1.3rem"
+                slides={[
+                    {
+                        backgroundImage:
+                            "/hall-of-fame/best-thesis-category/best-thesis-image-processing.jpg",
+                        ranking: "",
+                        group: "Array Co.",
+                        thesis: "DASH: Composite Restoration Using Image Recognition for Teeth Shade Matching Using Deep Learning",
+                        link: "/projects/Array-Co",
+                    },
+                ]}
+                id="carousel-best-thesis-image-processing"
+                carouselNumber={6}
+                carouselUp=""
+                carouselDown=""
+                carouselUpTarget="carousel-best-thesis-iot"
+                carouselDownTarget="carousel-best-thesis-education"
+                carouselLeft="hidden"
+                carouselRight="hidden"
+            />
+
+            <Carousel
+                title="BEST THESIS IN EDUCATION"
+                titleSize="1.3rem"
+                slides={[
+                    {
+                        backgroundImage:
+                            "/hall-of-fame/best-thesis-category/best-thesis-education.jpg",
+                        ranking: "",
+                        group: "Rob_lux",
+                        thesis: "TalkBox: Constraint-Based Intelligent Tutor for Augmentative and Alternative Communication for Filipino Preschoolers with Speech Delay: An Application of Decision Tree Algorithm",
+                        link: "/projects/Rob_lux",
+                    },
+                ]}
+                id="carousel-best-thesis-education"
+                carouselNumber={7}
+                carouselUp=""
+                carouselDown=""
+                carouselUpTarget="carousel-best-thesis-image-processing"
+                carouselDownTarget="carousel-best-thesis-data-analytics"
+                carouselLeft="hidden"
+                carouselRight="hidden"
+            />
+
+            <Carousel
+                title="BEST THESIS IN DATA ANALYTICS"
+                titleSize="1.3rem"
+                slides={[
+                    {
+                        backgroundImage:
+                            "/hall-of-fame/best-thesis-category/best-thesis-data-analytics.jpg",
+                        ranking: "",
+                        group: "return_to_sleep",
+                        thesis: "TomatoCare: A supply and demand forecasting support tool for tomatoes using ARIMA modeling",
+                        link: "/projects/return-to_sleep",
+                    },
+                ]}
+                id="carousel-best-thesis-data-analytics"
+                carouselNumber={8}
+                carouselUp=""
+                carouselDown=""
+                carouselUpTarget="carousel-best-thesis-education"
+                carouselDownTarget="carousel-best-audio-visual-presentation"
+                carouselLeft="hidden"
+                carouselRight="hidden"
+            />
+
+            <Carousel
+                title="BEST AUDIO-VISUAL PRESENTATION"
+                titleSize="1.3rem"
+                slides={[
+                    {
+                        backgroundImage:
+                            "/hall-of-fame/best-audio-visual-presentation/1st-place.jpg",
+                        ranking: "1st Place",
+                        group: "Quinta",
+                        thesis: "Luid: Language Revival: Gamifying Kapampangan Using Leitner System and SuperMemo Algorithms",
+                        link: "/projects/Quinta",
+                    },
+                    {
+                        backgroundImage:
+                            "/hall-of-fame/best-audio-visual-presentation/2nd-place.jpg",
+                        ranking: "2nd Place",
+                        group: "STARK",
+                        thesis: "VIDEO LENS: Video Content Analysis through Computer Vision and Natural Language Processing",
+                        link: "/projects/S.T.A.R.K.",
+                    },
+                    {
+                        backgroundImage:
+                            "/hall-of-fame/best-audio-visual-presentation/3rd-place.jpg",
+                        ranking: "3rd Place",
+                        group: "Array Co.",
+                        thesis: "DASH: Composite Restoration Using Image Recognition for Teeth Shade Matching Using Deep Learning",
+                        link: "/projects/Array-Co",
+                    },
+                ]}
+                id="carousel-best-audio-visual-presentation"
+                carouselNumber={9}
+                carouselUp=""
+                carouselDown=""
+                carouselUpTarget="carousel-best-thesis-data-analytics"
+                carouselDownTarget="carousel-best-poster"
+                carouselLeft=""
+                carouselRight=""
+            />
+
+            <Carousel
+                title="BEST POSTER"
+                titleSize="1.5rem"
+                slides={[
+                    {
+                        backgroundImage:
+                            "/hall-of-fame/best-poster/1st-place-1.jpg",
+                        ranking: "1st Place",
+                        group: "Engeenz",
+                        thesis: "RECOGNIZANCE: An Instagram-Based Recommender System using Naïve Bayes Classifier with TensorFlow for T-Cup-Zone in the City of Manila",
+                        link: "/projects/Engeenz",
+                    },
+                    {
+                        backgroundImage:
+                            "/hall-of-fame/best-poster/1st-place-2.jpg",
+                        ranking: "1st Place",
+                        group: "DEoS",
+                        thesis: "RevSentinel: A Browser Extension for Identifying Relevant Product Reviews on Online Shopping Platforms Using Image and Natural Language Processing Algorithms",
+                        link: "/projects/DEoS",
+                    },
+                    {
+                        backgroundImage:
+                            "/hall-of-fame/best-poster/2nd-place.jpg",
+                        ranking: "2nd Place",
+                        group: "Rob_lux",
+                        thesis: "TalkBox: Constraint-Based Intelligent Tutor for Augmentative and Alternative Communication for Filipino Preschoolers with Speech Delay: An Application of Decision Tree Algorithm",
+                        link: "/projects/Rob_lux",
+                    },
+                    {
+                        backgroundImage:
+                            "/hall-of-fame/best-poster/3rd-place.jpg",
+                        ranking: "3rd Place",
+                        group: "Hello World",
+                        thesis: "BLOODLINE: An Emergency Blood Resource Locator Implemented Using Contraction Hierarchies In OSRMV1 and GPS Tracking",
+                        link: "/projects/Hello-World",
+                    },
+                ]}
+                id="carousel-best-poster"
+                carouselNumber={10}
+                carouselUp=""
+                carouselDown=""
+                carouselUpTarget="carousel-best-audio-visual-presentation"
+                carouselDownTarget="carousel-best-mentor"
+                carouselLeft=""
+                carouselRight=""
+            />
+
+            <Carousel
+                title="BEST MENTOR"
+                titleSize="1.5rem"
+                slides={[
+                    {
+                        backgroundImage:
+                            "/hall-of-fame/best-mentor/best-mentor.jpg",
+                        ranking: "",
+                        group: "Ms. Elisa Malasaga",
+                        thesis: "Computer Science Department",
+                        link: "https://www.facebook.com/photo/?fbid=748468940629661&set=a.450638063746085",
+                    },
+                ]}
+                id="carousel-best-mentor"
+                carouselNumber={11}
+                carouselUp=""
+                carouselDown="hidden"
+                carouselUpTarget="carousel-best-poster"
+                carouselDownTarget=""
+                carouselLeft="hidden"
+                carouselRight="hidden"
             />
         </main>
     );
