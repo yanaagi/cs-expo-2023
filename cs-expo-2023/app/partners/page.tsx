6c5ca4593ca9b3698da3a08cd896a9c615e4ceec
Jeri-Michael Joseph Advincula <swordfartonline11@gmail.com>
2023-12-05 09:18:36 +0800

    partners

--- cs-expo-2023/app/partners/page.tsx
+++ cs-expo-2023/app/partners/page.tsx
@@ -1,47 +1,79 @@
+'use client'
 import React from 'react';
 import { NextPage } from "next";
 import PartnerIconsLayout from "../../components/PartnerIconsLayout";
 import AlternatingPartnersLayout from '../../components/AlternatingPartnersLayout';
-import partnersData from '../../partners.json';
+import goldData from '../../goldSpons.json';
+import bronzeData from '../../bronzeSpons.json';
 import CircularSidebar from '@/components/PartnersSidebar';
+import Nodesc from '../../components/AlternatingnonamePartnersLayout';
+import majorData from '../../majorPartner.json'
+import minorData from '../../minorPartners.json'
+import mediaData from '../../mediaPartners.json'
+
 
 const Partners: NextPage = () => {
-  const partnerArray = Object.values(partnersData);
-  const partners = partnerArray.map(partnersData => partnersData.imagePath); // Assuming 'imagePath' is the correct property
+  const goldArray = Object.values(goldData);
+  const bronzeArray = Object.values(bronzeData);
+  const majorArray = Object.values(majorData);
+  const minorArray = Object.values(minorData);
+  const mediaArray = Object.values(mediaData);
+
+  const allPartnersArray = [
+    ...goldArray,
+    ...bronzeArray,
+    ...mediaArray,
+    ...majorArray,
+    ...minorArray,
+  
+  ];
 
   return (
     <main className="p-24">
 <div className="ms-28 ml-[-4rem] sm:ml-0 lg:ml-28">
   <h1 className="font-regular text-[clamp(48px,3vw,72px)] m-0 mb-[-2.5rem] sm:mb-0 lg:mb-[-2.5rem]">our</h1>
   <h2 className="custom-font text-[clamp(48px,10vw,72px)] m-0 mt-[-1rem] sm:mt-0 lg:mt-[-1rem]">PARTNERS</h2>
-  <div className="flex items-center justify-center p-24">
-    <PartnerIconsLayout partners={partnerArray} />
-  </div>
-</div>
 
+  <div className="">
+          <PartnerIconsLayout partners={allPartnersArray} />
+      </div>
+
+</div>
+        <section id="gold-spons">
+        <h2 id="gold" className="custom-font text-[clamp(48px,5vw,72px)] m-0  flex justify-center">GOLD SPONSORS</h2>
+        </section>
+        <div>
+          <AlternatingPartnersLayout partners={goldArray} />
+        </div>
+        <section id="bronze-spons">
+        <h2 id="bronze" className="custom-font text-[clamp(48px,5vw,72px)] m-0  flex justify-center">BRONZE SPONSORS</h2>
+        </section>
+        <div>
+          <AlternatingPartnersLayout partners={bronzeArray} />
+        </div>
+        <div>
+          <section id="partners-media">
+          <h2 id="media" className="custom-font text-[clamp(48px,5vw,72px)] m-0  flex justify-center">MEDIA PARTNERS</h2>
+          </section>
+          <div>
+            <Nodesc partners={mediaArray} />
+          </div>
+        </div>
         <section id="partners-major">
         <h2 id="major" className="custom-font text-[clamp(48px,5vw,72px)] m-0  flex justify-center">MAJOR PARTNERS</h2>
         </section>
         <div>
-          <AlternatingPartnersLayout partners={partnerArray} />
+          <Nodesc partners={majorArray} />
         </div>
         <div>
         <section id="partners-minor">
           <h2 id="minor" className="custom-font text-9xl m-0 mt-[-1rem] flex justify-center p-24">MINOR PARTNERS</h2>
         </section>
           <div>
-            <AlternatingPartnersLayout partners={partnerArray} />
+            <Nodesc partners={minorArray} />
           </div>
         </div>
-        <div>
-          <section id="partners-media">
-          <h2 id="media" className="custom-font text-9xl m-0 mt-[-1rem] flex justify-center p-24">MEDIA PARTNERS</h2>
-          </section>
-          <div>
-            <AlternatingPartnersLayout partners={partnerArray} />
-          </div>
-        </div>
-      <CircularSidebar />
+        
     </main>
   );
 }
---
+++ cs-expo-2023/components/AlternatingnonamePartnersLayout.tsx
@@ -0,0 +1,111 @@
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react';
+import { useClient } from 'next/react-dev-overlay';
+
+
+interface PartnersLayoutProps {
+  partner: {
+    name: string;
+    facebook: string;
+    imagePath?: string; // Add imagePath as an optional property
+  };
+}
+
+const PartnersLeftLayout: React.FC<PartnersLayoutProps> = ({ partner }) => {
+  const backgroundImageStyle = partner.imagePath
+    ? { backgroundImage: `url(${partner.imagePath})`, backgroundSize: 'cover' }
+    : undefined;
+
+return (
+  <div className="flex items-center p-12 flex-col md:flex-row">
+    <div className="flex items-center">
+      {partner.imagePath && (
+        <div
+          className="flex-shrink-0 w-48 h-48 bg-blue-500"
+          style={backgroundImageStyle}
+        />
+      )}
+      <div className="flex flex-col items-start ml-2 md:ml-4">
+        <a href={partner.facebook} target="_blank" className="w-9 h-9 bg-coral-pink rounded-full mb-2 grid place-items-center"> 
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" className="bi bi-facebook" viewBox="0 0 16 16">
+            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+          </svg>
+        </a>
+        
+      </div>
+    </div>
+    <div className="flex flex-col ml-4">
+      <div className="font-bold text-2xl">{partner.name}</div>
+    </div>
+  </div>
+);
+      };
+const PartnersRightLayout: React.FC<PartnersLayoutProps> = ({ partner }) => {
+  const backgroundImageStyle = partner.imagePath
+    ? { backgroundImage: `url(${partner.imagePath})`, backgroundSize: 'cover' }
+    : undefined;
+
+    return (
+        <div className="justify-end flex items-center p-12 flex-col md:flex-row">
+          <div className="flex flex-col mr-4">
+            <div className="font-bold text-2xl">{partner.name}</div>
+          </div>
+          <div className="flex items-center">
+            <a href={partner.facebook} target="_blank" className="w-9 h-9 bg-coral-pink rounded-full mb-2 grid place-items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" className="bi bi-facebook" viewBox="0 0 16 16">
+                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+              </svg>
+            </a>
+          </div>
+          {partner.imagePath && (
+            <div
+              className="flex-shrink-0 w-48 h-48 mr-4"
+              style={backgroundImageStyle}
+            />
+          )}
+        </div>
+      );
+    
+    
+    
+};
+
+const useWindowSize = () => {
+  const [size, setSize] = useState([0, 0]);
+
+  useEffect(() => {
+    const updateSize = () => {
+      setSize([window.innerWidth, window.innerHeight]);
+    };
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+
+  return size;
+};
+
+interface AlternatingPartnersLayoutProps {
+  partners: Array<any>;
+}
+
+const AlternatingnonamePartnersLayout: React.FC<AlternatingPartnersLayoutProps> = ({ partners }) => {
+  const [width] = useWindowSize();
+  const isMobile = width < 768;
+
+  const alternatingPartners = partners.map((partner, index) => {
+    if (isMobile) {
+      return <PartnersRightLayout key={index} partner={partner} />;
+    } else {
+      if ((index + 1) % 2 === 1) {
+        return <PartnersLeftLayout key={index} partner={partner} />;
+      } else {
+        return <PartnersRightLayout key={index} partner={partner} />;
+      }
+    }
+  });
+
+  return <>{alternatingPartners}</>;
+};
+
+export default AlternatingnonamePartnersLayout;
