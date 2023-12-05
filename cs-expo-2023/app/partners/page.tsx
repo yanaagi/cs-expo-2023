f0590c79d8923fe504a8f155867017ab766d6ca7
LimyuA <92149293+LimyuA@users.noreply.github.com>
2023-12-05 12:31:19 +0800

    helo

--- cs-expo-2023/app/partners/page.tsx
+++ cs-expo-2023/app/partners/page.tsx
@@ -29,45 +29,49 @@ const Partners: NextPage = () => {
   ];
 
   return (
-    <main className="p-24">
-<div className="ms-28 ml-[-4rem] sm:ml-0 lg:ml-28">
-  <h1 className="font-regular text-[clamp(48px,3vw,72px)] m-0 mb-[-2.5rem] sm:mb-0 lg:mb-[-2.5rem]">our</h1>
-  <h2 className="custom-font text-[clamp(48px,10vw,72px)] m-0 mt-[-1rem] sm:mt-0 lg:mt-[-1rem]">PARTNERS</h2>
+    <main className="">
+
+<div className="custom-font text-[clamp(48px,3vw,72px)] m-0 flex flex-col items-start sm:items-center lg:items-start mt-32 ml-12 ">
+  <h1 className="custom-font text-[clamp(35px,10vw,72px)] m-0">our</h1>
+  <h2 className="custom-font text-[clamp(38px,10vw,72px)] m-0">PARTNERS</h2>
+</div>
+
+
 
   <div className="">
           <PartnerIconsLayout partners={allPartnersArray} />
       </div>
 
-</div>
+
         <section id="gold-spons">
-        <h2 id="gold" className="custom-font text-[clamp(48px,5vw,72px)] m-0  flex justify-center">GOLD SPONSORS</h2>
+        <h2 id="gold" className="custom-font text-[clamp(43px,5vw,72px)] m-0  flex justify-center ml-3">GOLD SPONSORS</h2>
         </section>
         <div>
           <AlternatingPartnersLayout partners={goldArray} />
         </div>
         <section id="bronze-spons">
-        <h2 id="bronze" className="custom-font text-[clamp(48px,5vw,72px)] m-0  flex justify-center">BRONZE SPONSORS</h2>
+        <h2 id="bronze" className="custom-font text-[clamp(43px,5vw,72px)] m-0  flex justify-center ml-3">BRONZE SPONSORS</h2>
         </section>
         <div>
           <AlternatingPartnersLayout partners={bronzeArray} />
         </div>
         <div>
           <section id="partners-media">
-          <h2 id="media" className="custom-font text-[clamp(48px,5vw,72px)] m-0  flex justify-center">MEDIA PARTNERS</h2>
+          <h2 id="media" className="custom-font text-[clamp(43px,5vw,72px)] m-0  flex justify-center ml-3">MEDIA PARTNERS</h2>
           </section>
           <div>
             <Nodesc partners={mediaArray} />
           </div>
         </div>
         <section id="partners-major">
-        <h2 id="major" className="custom-font text-[clamp(48px,5vw,72px)] m-0  flex justify-center">MAJOR PARTNERS</h2>
+        <h2 id="major" className="custom-font text-[clamp(43px,5vw,72px)] m-0  flex justify-center ml-3">MAJOR PARTNERS</h2>
         </section>
         <div>
           <Nodesc partners={majorArray} />
         </div>
         <div>
         <section id="partners-minor">
-          <h2 id="minor" className="custom-font text-9xl m-0 mt-[-1rem] flex justify-center p-24">MINOR PARTNERS</h2>
+          <h2 id="minor" className="custom-font text-[clamp(43px,5vw,72px)] m-0  flex justify-center ml-3">MINOR PARTNERS</h2>
         </section>
           <div>
             <Nodesc partners={minorArray} />
